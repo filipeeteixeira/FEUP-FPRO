7092c98071359be918fd58d4629eb79aec892f43
Filipe <filipeteixeira00@hotmail.com>
2019-10-10 00:42:27 +0100

    Exercises to practice

---
+++ FPROPlay/Py02/CircleArea.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 00:29:56 2019
+
+@author: filipe
+"""
+
+pi = 3.14159
+
+r = input()
+
+area = pi*pow(r,2)
+
+print(round(area,2))
