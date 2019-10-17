2bb8066a8f067ea26d60f9442fd7abc3e53582c8
Filipe <filipeteixeira00@hotmail.com>
2019-10-17 15:38:27 +0100

    practice

--- FPROPlay/Py03/FlowDiagram.py
+++ FPROPlay/Py03/FlowDiagram.py
@@ -6,15 +6,28 @@ Created on Sun Oct 13 19:51:47 2019
 @author: filipe
 """
 
-L = input()
-S = input()
+L = int(input())
+S = int(input())
 
 R=L
 
 if(R>S):
-
+    while(S<=R):
+        R=R-S
+        if S>R:
+            if R!=0:
+                L=R
+                R=S
+                S=L
 else:
     L=R
     R=S
     S=L
-    
+    while(S<=R):
+        R=R-S
+        if S>R:
+            if R!=0:
+                L=R
+                R=S
+                S=L
+print(S)
